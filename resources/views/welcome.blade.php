a6652b890b426efb6f5d850d0c12fb8fc59401ae
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-06-27 15:25:39 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+    <title>LARAVEL</title>
+</head>
+
+<body>
+    <h1 class="m-0 bg-success text-center">BENVENUTO IN LARAVEL</h1>
+</body>
+
+</html>
